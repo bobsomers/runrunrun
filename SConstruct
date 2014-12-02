484df1486e73c0afe4d76063eb92b520bf70f377
Bob Somers <bob@bobsomers.com>
2014-12-01 23:30:36 -0800

    Use GLFW to open a window.

--- SConstruct
+++ SConstruct
@@ -1,3 +1,5 @@
 env = Environment()
 
+env.ParseConfig('pkg-config --cflags --libs glfw3')
+
 env.Program('runrunrun', Glob('src/*.cpp'))
--- src/runrunrun.cpp
+++ src/runrunrun.cpp
@@ -1,6 +1,30 @@
+#include <GLFW/glfw3.h>
+
+#include <cstdlib>
 #include <iostream>
 
 int main() {
-    std::cout << "Run? Run. Run!" << std::endl;
-    return 0;
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Run? Run. Run!", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create window." << std::endl;
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        // TODO: render!
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return EXIT_SUCCESS;
 }
